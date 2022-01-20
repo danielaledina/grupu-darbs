8727995b8fee4bbbcb219e7024ebdf615a7dc108
Daniela <98079730+danielaledina@users.noreply.github.com>
2022-01-20 07:58:21 +0000

    viss pa jaunu

--- main.py
+++ main.py
@@ -1,19 +1,16 @@
-# Es esmu Armands
-
 import math
 
 def gridas_izmaksa(cena, linoleja_platums, telpas_platums, telpas_garums):
     telpas_izmers = math.ceil(telpas_garums) * math.ceil(telpas_platums)
     izmaksa = cena * telpas_izmers / linoleja_platums
-
     return izmaksa
 
-cena1 = 2.25
-linoleja_platums1 = 2.0
-platums1 = 5.25
-garums1 = 6.0
+cena = int(input("ievadi linoleja cenu eur uz kvadratmetru: "))
+linoleja_platums = int(input("ievadi linoleja ruļļa platumu metros: "))
+telpas_platums = int(input("ievadi telpas platumu metros: "))
+telpas_garums = int(input("ievadi telpas garumu metros: "))
 
 print("izklājot garumā:")
-print(gridas_izmaksa(cena1, linoleja_platums1, platums1, garums1))
+print(str(gridas_izmaksa(cena, linoleja_platums, telpas_platums, telpas_garums)) + " euro")
 print("izklājot platumā")
-print(gridas_izmaksa(cena1, linoleja_platums1, garums1, platums1))
+print(str(gridas_izmaksa(cena, linoleja_platums, telpas_garums, telpas_platums)) + " euro") 
